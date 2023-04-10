160e848b20bf41f78640494349739eec8ff36cfd
PauloRossi <paulogustavorossi@gmail.com>
2023-04-10 00:00:26 -0300

    tentando corrigir checkout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98670069b78b5e2e"; }
+/******/ 		__webpack_require__.h = function() { return "03d46cea96e91670"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/checkout/[id].page.tsx
+++ pages/checkout/[id].page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { Button, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Typography,
+  CardMedia,
+  Paper,
+  Container,
+} from "@mui/material";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Alert } from "@mui/material";
+import { getComic } from "dh-marvel/services/marvel/marvel.service";
+import { GetServerSideProps, NextPage } from "next";
+import { Comic } from "types/typeComics";
 
 type FormValues = {
   name: string;
@@ -18,7 +28,12 @@ type FormValues = {
   cardName: string;
   expirationDate: string;
   securityCode: string;
+  id: number;
 };
+
+interface Props {
+  data: Comic;
+}
 
 const userMessageSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
@@ -35,7 +50,7 @@ const userMessageSchema = Yup.object().shape({
   securityCode: Yup.string().required("Security Code is required"),
 });
 
-export default function Checkout(data: any) {
+const Checkout: NextPage<Props> = ({ data }: Props) => {
   const {
     register,
     handleSubmit,
@@ -43,6 +58,8 @@ export default function Checkout(data: any) {
   } = useForm<FormValues>({
     resolver: yupResolver(userMessageSchema),
   });
+
+  console.log(data);
 
   const createMessageRequest: SubmitHandler<FormValues> = (data) => {
     console.log(data);
@@ -55,262 +72,287 @@ export default function Checkout(data: any) {
     <>
       <main>
         <form onSubmit={handleSubmit(createMessageRequest)}>
-          <h1>Personal information</h1>
-          <TextField
-            id="filled-basic"
-            label="Name"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("name")}
-          />
-          <span>
-            {errors.name && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.name.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Last Name"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("lastName")}
-          ></TextField>
-          <span>
-            {errors.lastName && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.lastName.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Email"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("email")}
-          ></TextField>
-          <span>
-            {errors.email && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.email.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <h1>Delivery address</h1>
-          <TextField
-            id="filled-basic"
-            label="Address"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("address")}
-          ></TextField>
-          <span>
-            {errors.address && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.address.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Department"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("department")}
-          ></TextField>
-          <span>
-            {errors.department && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.department.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="City"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("city")}
-          ></TextField>
-          <span>
-            {errors.city && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.city.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Province"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("province")}
-          ></TextField>
-          <span>
-            {errors.province && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.province.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Zip Code"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("zipCode")}
-          ></TextField>
-          <span>
-            {errors.zipCode && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.zipCode.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <h1>Payment details</h1>
-          <TextField
-            id="filled-basic"
-            label="Card Number"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("cardNumber")}
-          ></TextField>
-          <span>
-            {errors.cardNumber && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.cardNumber.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Card Name"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("cardName")}
-          ></TextField>
-          <span>
-            {errors.cardName && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.cardName.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Expiration Date"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("expirationDate")}
-          ></TextField>
-          <span>
-            {errors.expirationDate && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.expirationDate.message}
-              </Alert>
-            )}
-          </span>
-          <br />
-          <TextField
-            id="filled-basic"
-            label="Security Code"
-            type="password"
-            variant="filled"
-            size="small"
-            sx={{ width: 300 }}
-            {...register("securityCode")}
-          ></TextField>
-          <span>
-            {errors.securityCode && (
-              <Alert
-                sx={{
-                  width: 300,
-                }}
-                severity="error"
-              >
-                {errors.securityCode.message}
-              </Alert>
-            )}
-          </span>
-          <br />
+          <div className="container">
+            <h1>Personal information</h1>
+            <TextField
+              id="filled-basic"
+              label="Name"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("name")}
+            />
+            <span>
+              {errors.name && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.name.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Last Name"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("lastName")}
+            ></TextField>
+            <span>
+              {errors.lastName && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.lastName.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Email"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("email")}
+            ></TextField>
+            <span>
+              {errors.email && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.email.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <h1>Delivery address</h1>
+            <TextField
+              id="filled-basic"
+              label="Address"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("address")}
+            ></TextField>
+            <span>
+              {errors.address && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.address.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Department"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("department")}
+            ></TextField>
+            <span>
+              {errors.department && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.department.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="City"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("city")}
+            ></TextField>
+            <span>
+              {errors.city && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.city.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Province"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("province")}
+            ></TextField>
+            <span>
+              {errors.province && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.province.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Zip Code"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("zipCode")}
+            ></TextField>
+            <span>
+              {errors.zipCode && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.zipCode.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <h1>Payment details</h1>
+            <TextField
+              id="filled-basic"
+              label="Card Number"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("cardNumber")}
+            ></TextField>
+            <span>
+              {errors.cardNumber && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.cardNumber.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Card Name"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("cardName")}
+            ></TextField>
+            <span>
+              {errors.cardName && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.cardName.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Expiration Date"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("expirationDate")}
+            ></TextField>
+            <span>
+              {errors.expirationDate && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.expirationDate.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+            <TextField
+              id="filled-basic"
+              label="Security Code"
+              type="password"
+              variant="filled"
+              size="small"
+              sx={{ width: 300 }}
+              {...register("securityCode")}
+            ></TextField>
+            <span>
+              {errors.securityCode && (
+                <Alert
+                  sx={{
+                    width: 300,
+                  }}
+                  severity="error"
+                >
+                  {errors.securityCode.message}
+                </Alert>
+              )}
+            </span>
+            <br />
+          </div>
+          {/* <Container maxWidth="sm" sx={{ marginTop: "30px" }}>
+            <Paper
+              sx={{
+                padding: 2,
+                display: "flex",
+                flexDirection: "column",
+                rowGap: 2,
+              }}
+              elevation={3}
+            >
+              <CardMedia
+                component="img"
+                src={`${data.thumbnail.path}.${data.thumbnail.extension}`}
+                sx={{ width: "200px" }}
+              />
+              <Typography fontSize={18}>
+                <strong>Título:</strong> {data.title}
+              </Typography>
+              <Typography fontSize={18}>
+                <strong>Preço:</strong> {data.price}
+              </Typography>
+            </Paper>
+          </Container> */}
           <Button
             variant="contained"
             sx={{ width: 300, marginTop: 2 }}
@@ -322,4 +364,17 @@ export default function Checkout(data: any) {
       </main>
     </>
   );
-}
+};
+
+export default Checkout;
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id } = query;
+  const comic = await getComic(Number(id));
+
+  return {
+    props: {
+      comic,
+    },
+  };
+};
